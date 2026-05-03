78fe57157e1565c85e5e9f483271e8e4e419c6ab
rzambrano1 <rzambrano@gmail.com>
2026-05-03 10:53:40 -0400

    Start setup for vector db

--- src/data/ingest.py
+++ src/data/ingest.py
@@ -3,6 +3,7 @@ A simple evaluation pipeline to ingest corpora to be used in a RAG (Retrieval-Au
 """
 
 # Session Setup
+# -------------
 
 # Boilerplate Modules
 import os
@@ -15,6 +16,9 @@ import time
 import logging
 from tqdm import tqdm
 
+# Data wrangling and scientific computing modules
+import numpy as np
+
 # Config Files
 import hydra
 from hydra.utils import get_original_cwd
@@ -23,6 +27,16 @@ from omegaconf import DictConfig, OmegaConf
 # Ingestion tooling
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from sentence_transformers import SentenceTransformer
+
+# Vector Databases
+
+import chromadb
+import lancedb
+import pyarrow as pa
+from rank_bm25 import BM25Okap
+
+# Data Wrangling
+# --------------
 
 # Initializing Logger
 logger = logging.getLogger(__name__)
@@ -33,7 +47,7 @@ logger = logging.getLogger(__name__)
 # Leaderboard available at https://huggingface.co/spaces/mteb/leaderboard 
 model = SentenceTransformer("BAAI/bge-small-en-v1.5") # https://bge-model.com/bge/bge_v1_v1.5.html
 
-def ingest_arxiv_files(langchain_text_splliter):
+def ingest_arxiv_files(langchain_text_spliter):
 
     logger.info('Preprocessing Arxiv files: chunking and embedding encoding... ')
 
@@ -63,7 +77,7 @@ def ingest_arxiv_files(langchain_text_splliter):
                     print(file_path)
                     print(entry_text)
 
-                    topic_entry_chunks = langchain_text_splliter.split_text(entry_text)
+                    topic_entry_chunks = langchain_text_spliter.split_text(entry_text)
 
                     print("-"*50)
 
@@ -81,7 +95,7 @@ def ingest_arxiv_files(langchain_text_splliter):
 
     return arxiv_chunks, arxiv_chunk_embeddings
 
-def ingest_wiki_files(langchain_text_splliter):
+def ingest_wiki_files(langchain_text_spliter):
 
     logger.info('Preprocessing Wikipedia files: chunking and embedding encoding... ')
 
@@ -104,7 +118,7 @@ def ingest_wiki_files(langchain_text_splliter):
                 print('path: ',file_path)
                 print('entry: ', entry_text)
 
-                topic_entry_chunks = langchain_text_splliter.split_text(entry_text)
+                topic_entry_chunks = langchain_text_spliter.split_text(entry_text)
 
                 print("-"*50)
 
@@ -122,7 +136,7 @@ def ingest_wiki_files(langchain_text_splliter):
 
     return wiki_chunks, wiki_chunk_embeddings
 
-def ingest_firecrawl_files(langchain_text_splliter):
+def ingest_firecrawl_files(langchain_text_spliter):
 
     logger.info('Preprocessing Firecrawl files: chunking and embedding encoding... ')
 
@@ -156,7 +170,7 @@ def ingest_firecrawl_files(langchain_text_splliter):
                     entry_text = data['markdown']
                     print(entry_text)
                     
-                    topic_entry_chunks = langchain_text_splliter.split_text(entry_text)
+                    topic_entry_chunks = langchain_text_spliter.split_text(entry_text)
 
                     print("-"*50)
 
@@ -174,6 +188,39 @@ def ingest_firecrawl_files(langchain_text_splliter):
 
     return firecrawl_chunks, firecrawl_chunk_embeddings
 
+def ingest_firecrawl_files(langchain_text_spliter, bank):
+    
+    if bank not in ["JPM", "COF"]:
+        raise ValueError("Allowed values for bank are: ['JPM', 'COF']")
+
+    logger.info(f'Preprocessing SEC 10-K filings files for {bank}: chunking and embedding encoding... ')
+
+    notebook_dir = Path.cwd()
+
+    sec_dir = notebook_dir.parent / "raw" / "sec" 
+    txt_files = list(sec_dir.rglob("*.txt"))
+
+    bank_files = [file for file in txt_files if bank in str(file)]
+
+    for file_path in bank_files:
+
+        with open(file_path, 'r', encoding='utf-8') as file:
+            
+            text_content = file.read()
+            print(f'10-K file submission length: {text_content}')
+            print(text_content[100])
+
+            bank_entry_chunks = langchain_text_spliter.split_text(text_content)
+
+            bank_chunk_embeddings = model.encode(
+                bank_entry_chunks,
+                normalize_embeddings=True,  # important for cosine similarity
+                show_progress_bar=True
+            )
+    
+    assert len(bank_entry_chunks) == len(bank_chunk_embeddings), "Oh No! The length of the SEC 10-K filings chunks are not matching embeddings..."
+
+    return bank_entry_chunks, bank_chunk_embeddings
 
 @hydra.main(version_base=None, config_path="../../conf", config_name="config")
 def main(cfg : DictConfig):
@@ -187,9 +234,23 @@ def main(cfg : DictConfig):
         length_function=cfg.ingest.length_function,
     )
 
+    # Chunking and embeddings
+
     arxiv_chunks, arxiv_chunk_embeddings = ingest_arxiv_files(text_splitter)
     wiki_chunks, wiki_chunk_embeddings = ingest_wiki_files(text_splitter)
     firecrawl_chunks, firecrawl_chunk_embeddings = ingest_firecrawl_files(text_splitter)
+
+    jpm_chunks, jmp_embeddings = ingest_firecrawl_files(text_splitter, "JPM")
+    cof_chunks, cof_embeddings = ingest_firecrawl_files(text_splitter, "COF")
+
+    combined_chunks = arxiv_chunks + wiki_chunks + firecrawl_chunks + jpm_chunks + cof_chunks
+    combined_embeddings = arxiv_chunk_embeddings + wiki_chunk_embeddings + firecrawl_chunk_embeddings + jmp_embeddings + cof_embeddings
+    
+    # Vector Databases
+
+    root_dir = Path.cwd()
+    chroma_dir = os.path.normpath(root_dir / cfg.ingest.CHROMA_DIR)
+    
 
 
 if __name__ == "__main__":
